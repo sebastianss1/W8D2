6f70301ba515360a11e0cb95b92021734fd8be9c
Sebastian Sanchez <sebastian@artxcode.io>
2021-04-13 17:04:31 -0400

    Phase 2 updates

--- bin/p08_exception_server.rb
+++ bin/p08_exception_server.rb
@@ -7,6 +7,7 @@ class StatusesController < ControllerBase
   def index
     statuses = $statuses.select do |s|
       s[:cat_id] == Integer(params['cat_id'])
+      ##
     end
 
     render_content(statuses.to_s, "text/text")
